a6006713c18212e6b7affd30ea3ab8adee9056ef
Shreya7078 <shreyajain7078@gmail.com>
2026-01-17 22:42:56 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 # it provides various functions and variables that are used to manipulate different parts of the Python runtime environment.
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,3 +18,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message 
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by zero")
+        raise CustomException(e,sys)
